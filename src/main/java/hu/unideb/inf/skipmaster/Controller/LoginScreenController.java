ffc91b4be9d1de5a18ee5f1ead667417fc80f793
Szathmári Gergő <szath.gergo@gmail.com>
2019-05-02 00:10:48 +0200

    Register and Login screen interaction added, communication with DB

--- src/main/java/hu/unideb/inf/skipmaster/Controller/LoginScreenController.java
+++ src/main/java/hu/unideb/inf/skipmaster/Controller/LoginScreenController.java
@@ -2,8 +2,11 @@
 package hu.unideb.inf.skipmaster.Controller;
 
 import java.io.IOException;
+import java.math.BigInteger;
 import java.net.URL;
+import java.security.MessageDigest;
 import java.util.ResourceBundle;
+import java.sql.*;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -27,12 +30,44 @@ public class LoginScreenController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) 
     {
         // TODO
+        
     }    
 
     @FXML
-    private void loginButtonPressed(ActionEvent event) 
+    private void loginButtonPressed(ActionEvent event)
     {
         //bejelentkezési ablak Belépés gombjának lenyomása
+        /*
+            Itt kéne megcsinálni az adabázisból a lekérdezést
+            
+        */
+        if(loginneptun.getText().isEmpty() || loginpwd.getText().isEmpty()){
+            System.out.println("A neptun-kód/jelszó mező nem lehet üres.");
+        }else{
+            try (Connection connection = DriverManager.getConnection("jdbc:mariadb://localhost/SkipMaster?user=root&password=asd123")) {
+                Statement stmt=connection.createStatement();
+                ResultSet rs=stmt.executeQuery("select passwd from user where neptunID = '" + loginneptun.getText() + "';");
+                if(rs.next()){
+                   MessageDigest md = MessageDigest.getInstance("MD5");
+                   byte[] buffer = md.digest(loginpwd.getText().getBytes());
+                   BigInteger no = new BigInteger(1, buffer); 
+                   String hashedPwd = no.toString(16); 
+                   while (hashedPwd.length() < 32) { 
+                       hashedPwd = "0" + hashedPwd; 
+                   }
+                   if(!(rs.getString(1).equals(hashedPwd))){
+                      System.out.println("Hibás jelszó"); //Ez mehet majd a GUI-ra
+                  }else{
+                      System.out.println("Bejelentkezve");
+                  }
+                }else{
+                    System.out.println("Hibás neptunID"); //Ez mehet majd a GUI-ra
+                }
+
+           }catch(Exception e){
+               System.out.println(e);
+           }
+        }
     }
 
     @FXML
--- src/main/java/hu/unideb/inf/skipmaster/Controller/RegisterScreenController.java
+++ src/main/java/hu/unideb/inf/skipmaster/Controller/RegisterScreenController.java
@@ -2,7 +2,13 @@
 package hu.unideb.inf.skipmaster.Controller;
 
 
+import java.math.BigInteger;
 import java.net.URL;
+import java.security.MessageDigest;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -10,25 +16,49 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 
-
 public class RegisterScreenController implements Initializable {
 
     @FXML
     private TextField registerneptun; //regisztrációs ablak neptunID mezője
     @FXML
     private PasswordField registerpwd;    //regisztrációs ablak jelszó mezője
-
+    
+    private String hashedPwd;
     
     @Override
     public void initialize(URL url, ResourceBundle rb) 
     {
-        // TODO
+        
     }    
 
     @FXML
     private void registerButtonPressed(ActionEvent event)
     {
         //regisztrációs ablak Regisztráció gombjának lenyomása
+        if(registerneptun.getText().isEmpty() || registerpwd.getText().isEmpty()){
+            System.out.println("A neptun-kód/jelszó mező nem lehet üres.");
+        }else{
+            try{
+                MessageDigest md = MessageDigest.getInstance("MD5");
+                byte[] buffer = md.digest(registerpwd.getText().getBytes());
+                BigInteger no = new BigInteger(1, buffer); 
+                hashedPwd = no.toString(16); 
+                while (hashedPwd.length() < 32) { 
+                    hashedPwd = "0" + hashedPwd; 
+                }
+                 try (Connection connection = DriverManager.getConnection("jdbc:mariadb://localhost/SkipMaster?user=root&password=asd123")) {
+                    Statement stmt=connection.createStatement();
+                    ResultSet rs=stmt.executeQuery("select neptunID from user where neptunID = '" + registerneptun.getText() + "';");
+                    if(rs.next()){
+                       System.out.println("Ez a neptun-kód már regisztrálva van.");
+                    }else{
+                        rs=stmt.executeQuery("insert into user(neptunID, passwd) values('" + registerneptun.getText() + "', '" + hashedPwd + "');");
+                    }
+                 }
+            }catch(Exception e){
+                System.out.println(e.getMessage());
+            }     
+        }
     }
     
     
