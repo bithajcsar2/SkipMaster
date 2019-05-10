c972f67715f91b09e02ca76c2c551e864d3bbd58
Szathmári Gergő <szath.gergo@gmail.com>
2019-05-10 21:44:51 +0200

    Adatok feltöltése a táblázatba bejelentzekés után. Fájltallózó implementálása.

--- src/main/java/hu/unideb/inf/skipmaster/Controller/LoginScreenController.java
+++ src/main/java/hu/unideb/inf/skipmaster/Controller/LoginScreenController.java
@@ -2,13 +2,10 @@
 package hu.unideb.inf.skipmaster.Controller;
 
 import java.io.BufferedReader;
-import java.io.FileInputStream;
 import java.io.FileReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.math.BigInteger;
 import java.net.URL;
-import java.nio.charset.Charset;
 import java.security.MessageDigest;
 import java.util.ResourceBundle;
 import java.sql.*;
@@ -32,6 +29,8 @@ public class LoginScreenController implements Initializable {
     private PasswordField loginpwd; //bejelentkezési ablak jelszó mezője
     @FXML
     private Button loginbutton;
+    
+    public static String userLoggedIn; //A bejelentkezett user felhasználóneve
  
     @Override
     public void initialize(URL url, ResourceBundle rb) 
@@ -48,7 +47,7 @@ public class LoginScreenController implements Initializable {
             Itt kéne megcsinálni az adabázisból a lekérdezést
             
         */
-        boolean successful_login = true; //hiba esetén is megnyílik a mainscreen; később vissza kell állítani falsera
+        boolean successful_login = false; //hiba esetén is megnyílik a mainscreen; később vissza kell állítani falsera
         
         if(loginneptun.getText().isEmpty() || loginpwd.getText().isEmpty()){
             System.out.println("A neptun-kód/jelszó mező nem lehet üres.");
@@ -68,16 +67,7 @@ public class LoginScreenController implements Initializable {
                         System.out.println("Hibás jelszó"); //Ez mehet majd a GUI-ra
                   }else{
                     System.out.println("Bejelentkezve");
-                    rs = stmt.executeQuery("select * from " + loginneptun.getText() + ";");
-                    if(rs.next()){
-                        System.out.println(rs.getString("course") + '(' + rs.getString("course_type") + ')');
-                        while(rs.next()){
-                           System.out.println(rs.getString("course") + '(' + rs.getString("course_type") + ')');
-                        }
-                   }else{
-                        System.out.println("Még nincsenek feltöltve a tárgyaid, tedd meg most! ;)");
-                        loadDataFromFile("targyak.csv", stmt); //Ide kell majd a path a fájlhoz a tallózásból.
-                   }
+                    userLoggedIn = loginneptun.getText();
                    successful_login = true;
                   }
                 }else{
@@ -101,7 +91,6 @@ public class LoginScreenController implements Initializable {
             Stage stage = (Stage) loginbutton.getScene().getWindow();
             stage.close();
         }
-
     }
 
     @FXML
@@ -115,69 +104,4 @@ public class LoginScreenController implements Initializable {
         stage.setScene(scene);
         stage.show();
     }
-    
-    private void loadDataFromFile(String path, Statement st){
-        try{
-            FileReader fr = new FileReader(path);
-            BufferedReader br = new BufferedReader(fr);
-            String line;
-            String targynev;
-            String[] buff;
-            char type = 'E';
-            while((line = br.readLine()) != null){
-                    buff = line.split(";");
-                    String[] tkod = buff[2].split("-");
-                    if(tkod.length <= 1)
-                        continue;
-                     
-                    for (String tkod1 : tkod) {
-                        tkod1 = tkod1.trim();
-                        if (tkod1.endsWith("E")) {
-                            type = 'E';
-                            break;
-                        } else if (tkod1.endsWith("G")) {
-                            type = 'G';
-                            break;
-                        } else if (tkod1.endsWith("L")) {
-                            type = 'L';
-                            break;
-                        }
-                    }
-                    int a = buff[2].indexOf('(');
-                    char[] charbuff = new char[a];
-                    buff[2].getChars(0, a, charbuff, 0);
-                    switch(type){
-                        case 'E':
-                            targynev = String.copyValueOf(charbuff);
-                            st.executeQuery("insert into " + loginneptun.getText() + "(course, course_type) values('" + targynev + "', 'előadás');");
-                            break;
-                        case 'G':
-                            targynev = String.copyValueOf(charbuff);
-                            st.executeQuery("insert into " + loginneptun.getText() + "(course, course_type) values('" + targynev + "', 'gyakorlat');");
-                            break;
-                        case 'L':
-                            targynev = String.copyValueOf(charbuff);
-                            st.executeQuery("insert into " + loginneptun.getText() + "(course, course_type) values('" + targynev + "', 'labor');");
-                            break;
-                    }
-                
-            }
-            br.close();
-        }catch(Exception e){
-            System.out.println(e);
-        }        
-    }
-    /*
-    Ez majd akkor kell, ha meglesz a felhasználói felület a tárgyakhoz
-    selectedCourse: amelyik tárgyat skipelte
-    selectedCourseType: előadás|gyakorlat|labor
-    private void increaseSkip(){
-        try (Connection connection = DriverManager.getConnection("jdbc:mariadb://localhost/SkipMaster?characterEncoding=UTF-8&user=root&password=asd123")) {
-            Statement stmt=connection.createStatement();
-            stmt.executeQuery("update " + loginneptun.getText() + " set numberOfSkips = numberOfSkips+1 where course = '" + selectedCourse + "' and course_type = '" + selectedCourseType + "';");
-        }catch(Exception e){
-            System.out.println(e);
-        }
-    }
-    */
 }
--- src/main/java/hu/unideb/inf/skipmaster/Controller/MainScreenController.java
+++ src/main/java/hu/unideb/inf/skipmaster/Controller/MainScreenController.java
@@ -7,16 +7,27 @@ package hu.unideb.inf.skipmaster.Controller;
  */
 
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.geometry.HPos;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.text.Text;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
@@ -47,6 +58,8 @@ public class MainScreenController implements Initializable {
     private Button filebrowsebutton;
     @FXML
     private Button importfromfilebutton;
+    @FXML
+    private GridPane table;
     
     File timetable; // ezt adja vissza a fájlmegnyitó alprogram
     
@@ -54,6 +67,7 @@ public class MainScreenController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) 
     {
         dayComboBox.getItems().addAll("Hétfő", "Kedd", "Szerda", "Csütörtök", "Péntek");
+        loadTable(false);
     }    
 
     @FXML
@@ -79,6 +93,7 @@ public class MainScreenController implements Initializable {
         timetable =  fileChooser.showOpenDialog(stage);
         filenamelabel.setText("Betallózott fájl: " + timetable.getName());
         importfromfilebutton.setDisable(false);
+         
     }
 
     @FXML
@@ -88,11 +103,48 @@ public class MainScreenController implements Initializable {
         {
             System.out.println("Hiba történt a fájl megynitása során!");
         }
-        
+        try (Connection connection = DriverManager.getConnection("jdbc:mariadb://localhost/SkipMaster?characterEncoding=UTF-8&user=root&password=asd123")) {
+            Statement stmt=connection.createStatement();
+            loadDataFromFile(timetable.getPath(), stmt);
+            stmt.close();
+            loadTable(true);
+        }catch(Exception e){
+            System.out.println(e);
+        }
         /*
             Ide kellene implementálni a fájlból importálást
             A fájlra a 'timetable' változóval tudsz hivatkozni
         */
+    }
+    
+    private void loadTable(boolean sync){
+        
+        try (Connection connection = DriverManager.getConnection("jdbc:mariadb://localhost/SkipMaster?characterEncoding=UTF-8&user=root&password=asd123")) {
+            Statement stmt=connection.createStatement();
+            ResultSet rs = stmt.executeQuery("select * from " + LoginScreenController.userLoggedIn + ";");
+            int i=0;
+            if(sync){
+                table.getChildren().clear();
+            }     
+            while(rs.next()){       
+                Label course = new Label(rs.getString("course"));
+                Label course_type = new Label(rs.getString("course_type"));
+                int remainingSkips = 3 - rs.getInt("numberOfSkips");
+                if(remainingSkips < 0)
+                    remainingSkips = 0;
+                Label numberOfSkips = new Label(Integer.toString(remainingSkips));
+                table.add(course, 0, i);
+                table.add(course_type, 1, i);
+                table.add(numberOfSkips, 2, i);
+                GridPane.setMargin(course, new Insets(0,0,0,5));
+                GridPane.setMargin(course_type, new Insets(0,0,0,5));
+                GridPane.setMargin(numberOfSkips, new Insets(0,0,0,5));
+                i++;
+            }
+            stmt.close();
+        }catch(Exception e){
+            System.out.println(e);
+        }
     }
     
     @FXML
@@ -102,5 +154,74 @@ public class MainScreenController implements Initializable {
             Szinkronizálás gomb nyomására hívódik meg. Lekérdezi az adatokat, és megjeleníti az UI-on. 
             Egyelőre nincs mit szinkronizálni XD
         */
+        loadTable(true);
+        
     }
+    
+     private void loadDataFromFile(String path, Statement st){
+        try{
+            FileReader fr = new FileReader(path);
+            BufferedReader br = new BufferedReader(fr);
+            String line;
+            String targynev;
+            String[] buff;
+            char type = 'E';
+            ResultSet rs = st.executeQuery("select * from " + LoginScreenController.userLoggedIn);
+            if(rs.next()){
+                st.executeQuery("drop table if exists " + LoginScreenController.userLoggedIn);
+                st.executeQuery("create table if not exists " + LoginScreenController.userLoggedIn + " (id int auto_increment, course varchar(100),course_type varchar(20), numberOfSkips int default 0, primary key(id));");
+            }
+            while((line = br.readLine()) != null){
+                    buff = line.split(";");
+                    String[] tkod = buff[2].split("-");
+                    if(tkod.length <= 1)
+                        continue;
+                     
+                    for (String tkod1 : tkod) {
+                        tkod1 = tkod1.trim();
+                        if (tkod1.endsWith("E")) {
+                            type = 'E';
+                            break;
+                        } else if (tkod1.endsWith("G")) {
+                            type = 'G';
+                            break;
+                        } else if (tkod1.endsWith("L")) {
+                            type = 'L';
+                            break;
+                        }
+                    }
+                    int a = buff[2].indexOf('(');
+                    char[] charbuff = new char[a];
+                    buff[2].getChars(0, a, charbuff, 0);
+                    switch(type){
+                        case 'E':
+                            targynev = String.copyValueOf(charbuff);
+                            st.executeQuery("insert into " +  LoginScreenController.userLoggedIn + "(course, course_type) values('" + targynev + "', 'előadás');");
+                            break;
+                        case 'G':
+                            targynev = String.copyValueOf(charbuff);
+                            st.executeQuery("insert into " +  LoginScreenController.userLoggedIn + "(course, course_type) values('" + targynev + "', 'gyakorlat');");
+                            break;
+                        case 'L':
+                            targynev = String.copyValueOf(charbuff);
+                            st.executeQuery("insert into " +  LoginScreenController.userLoggedIn + "(course, course_type) values('" + targynev + "', 'labor');");
+                            break;
+                    }
+            }
+            br.close();
+        }catch(Exception e){
+            System.out.println(e);
+        }        
+    }
+    
+     /*
+    private void Skipped(){
+        try (Connection connection = DriverManager.getConnection("jdbc:mariadb://localhost/SkipMaster?characterEncoding=UTF-8&user=root&password=asd123")) {
+            Statement stmt=connection.createStatement();
+            stmt.executeQuery("update " + LoginScreenController.userLoggedIn + " set numberOfSkips = numberOfSkips+1 where course = '" + selectedCourse + "' and course_type = '" + selectedCourseType + "';");
+        }catch(Exception e){
+            System.out.println(e);
+        }
+    }
+    */
 }
